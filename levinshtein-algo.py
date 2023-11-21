a7569acdc3f64299cb4641d6feb17ff34d33af6f
sameer6699 <sammyjadhav6699@gmail.com>
2023-11-21 23:31:37 +0530

    Second-Commit

---
+++ levinshtein-algo.py
@@ -0,0 +1,31 @@
+def levenshtein_distance(str1, str2):
+    len_str1 = len(str1) + 1
+    len_str2 = len(str2) + 1
+
+    # Create a matrix to store the distances
+    matrix = [[0] * len_str2 for _ in range(len_str1)]
+
+    # Initialize the matrix
+    for i in range(len_str1):
+        matrix[i][0] = i
+    for j in range(len_str2):
+        matrix[0][j] = j
+
+    # Fill in the matrix
+    for i in range(1, len_str1):
+        for j in range(1, len_str2):
+            cost = 0 if str1[i - 1] == str2[j - 1] else 1
+            matrix[i][j] = min(
+                matrix[i - 1][j] + 1,  # deletion
+                matrix[i][j - 1] + 1,  # insertion
+                matrix[i - 1][j - 1] + cost,  # substitution
+            )
+
+    # The bottom-right cell contains the Levenshtein distance
+    return matrix[len_str1 - 1][len_str2 - 1]
+
+# Example usage:
+str1 = "kitten"
+str2 = "sitting"
+distance = levenshtein_distance(str1, str2)
+print(f"The Levenshtein distance between '{str1}' and '{str2}' is {distance}.")
